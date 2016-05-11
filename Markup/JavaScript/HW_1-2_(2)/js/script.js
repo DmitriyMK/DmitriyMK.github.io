1825e5935b31c35114572f4e7b214985c2eff3dc
Dmitriy <alchimicus@gmail.com>
2016-05-12 01:39:11 +0300

    Done Hometask #1-2 JS
    Fixed mistakes

--- Markup/JavaScript/HW_1-2_(2)/js/script.js
+++ Markup/JavaScript/HW_1-2_(2)/js/script.js
@@ -1,29 +1,27 @@
-
 (function() {
 
-var arr = [];
+	var arr = [i];
 
-for (var i = 0; i < 5; i++) {
-	arr[i] = prompt('Введите имя пользователя','Mickey');
-    if (arr[i] == null){
-		return false;
-    }
-} 	
-console.log(i);
+	for (var i = 0; i < 5; i++) {
+		arr[i] = prompt('Введите имя пользователя','Mickey');
+		if (arr[i] === null){
+			alert('Таки необходимо было назвать имя пользователя');
+		} break;
+	} 	
 
-var userName = prompt('Введите свое имя','Mickey');
-var flag = false;
+	var userName = prompt('Введите свое имя','Mickey');
+	var flag = false;
 
-for (var i = 0; i < arr.length; i++) {
-	if (arr[i] === userName){
-		flag = true;
-  break;
-}
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === userName){
+			flag = true;
+			break;
+		}
+	}
 
-if (flag){
-	alert(userName + ' вы успешно вошли!');
-} else {
-	alert('Такого пользователя не существует!');
-}
-}
+	if (flag){
+		alert(userName + ' Таки вы успешно вошли!');
+	} else {
+		alert('Такого пользователя не существует!');
+	}
 }) ();
