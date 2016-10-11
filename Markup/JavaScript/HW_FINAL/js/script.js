1c3b9392fa081c29b16c867ab1f126d46395dacc
Dmitriy <alchimicus@gmail.com>
2016-10-12 00:23:47 +0300

    Change for pictures
    Change for pictures (still not adaptive)

--- Markup/JavaScript/HW_FINAL/js/script.js
+++ Markup/JavaScript/HW_FINAL/js/script.js
@@ -1,9 +1,28 @@
 // external js: masonry.pkgd.js
 
-$('.grid').masonry({
+/*$('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: 300,
   gutter: 20
-});
+});*/
+
+
+if($(window).width() < 939)  {
+   // change functionality for smaller screens
+   $('.grid').masonry({
+   	itemSelector: '.grid-item',
+   	columnWidth: 236,
+   	gutter: 20
+   });
+
+} else {
+   // change functionality for larger screens
+   $('.grid').masonry({
+   	itemSelector: '.grid-item',
+   	columnWidth: 300,
+   	gutter: 20
+   });
+}
+
 
 
