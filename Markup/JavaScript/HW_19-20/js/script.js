01a07baef19dfc9fb59207de2eda5d31cbc14f92
Dmitriy <alchimicus@gmail.com>
2016-10-11 22:06:19 +0300

    Done HW_19-20
    Done HW_19-20

---
+++ Markup/JavaScript/HW_19-20/js/script.js
@@ -0,0 +1,42 @@
+(function ($) {
+    $(function () {
+        $('.jcarousel').jcarousel({
+            animation: 'slow'
+        })
+
+        .jcarouselAutoscroll({
+            interval: 5000,
+            target: '+=1',
+            autostart: true
+        });
+
+        $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination({
+            'perPage': 1
+        });
+    });
+
+})(jQuery);
+
+$(function () {
+
+    $('.accordion__header').click(function (e) {
+
+        e.preventDefault();
+
+        $(this).siblings('.accordion-body').slideToggle()
+        .parent().toggleClass('active')
+        .siblings().removeClass('active')
+        .children('.accordion-body').slideUp();
+
+    });
+
+});
+  
+
