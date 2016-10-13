7a37f7b4f81c68f50b5d6809ba993f21f88631b6
Dmitriy <alchimicus@gmail.com>
2016-10-14 00:58:00 +0300

    Fixed HW
    Done adaptive and responsive.
    Without crossbrowser and some JS Effect

--- Markup/JavaScript/HW_FINAL/js/script.js
+++ Markup/JavaScript/HW_FINAL/js/script.js
@@ -1,19 +1,19 @@
 // external js: masonry.pkgd.js
 
-/*$('.grid').masonry({
+$('.grid').masonry({
   itemSelector: '.grid-item',
-  columnWidth: 300,
+  columnWidth: 236,
   gutter: 20
-});*/
+});
 
 
-if($(window).width() < 939)  {
+/*if($(window).width() > 939)  {
    // change functionality for smaller screens
    $('.grid').masonry({
    	itemSelector: '.grid-item',
    	columnWidth: 236,
    	gutter: 20
-   });
+   })
 
 } else {
    // change functionality for larger screens
@@ -22,7 +22,7 @@ if($(window).width() < 939)  {
    	columnWidth: 300,
    	gutter: 20
    });
-}
+}*/
 
 
 
