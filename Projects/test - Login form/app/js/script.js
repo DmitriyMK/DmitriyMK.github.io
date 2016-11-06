012c1fd2cfe020c8ba84cf2f606b2c943705fc3c
Dmitriy <alchimicus@gmail.com>
2016-11-07 00:25:56 +0200

    test
    done without 'hello username'

--- Projects/test - Login form/app/js/script.js
+++ Projects/test - Login form/app/js/script.js
@@ -2,36 +2,34 @@ function ValidateForm() {
 
   valid = true;
 
-  if (username.value.length == 0) {
-    document.getElementById('error__name').innerText = (" *Please enter your name ");
+  if (username.value == 0) {
+    document.getElementById('error__name').innerHTML = (" *Please enter your name ");
     username.focus();
     return false;
-  };
+  }
 
-  if (password.value.length == 0) {
-    document.getElementById('error__password').innerText = (" *Please enter your password ");
+  if (password.value == 0) {
+    document.getElementById('error__password').innerHTML = (" *Please enter your password ");
     password.focus();
     return false;
   }
 
   setTimeout(function() {
-    document.location.href = "main.html"
-  });
+    document.location.href = "main.html";
+  })
 
   return valid;
-
 }
 
-
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "" || user != "undefined") {
-        document.getElementById("greeting").innerHTML = "Welcome " + user;
-    }
-}
 
 
 function LogOut() {
+  function checkCookie() {
+    var name = document.username.value;
+    document.getElementById('greeting').innerHTML = ('hi' + name);
+  }
+
+
   setTimeout(function() {
     document.location.href = "index.html"
   });
