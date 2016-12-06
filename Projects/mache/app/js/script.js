0a4ff5acd9ee1a9cb936de971deb945b90b7213c
Dmitriy <alchimicus@gmail.com>
2016-12-06 22:22:51 +0200

    Added loader
    loader

--- Projects/mache/app/js/script.js
+++ Projects/mache/app/js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
 	$("#menu").on("click", "a", function(event) {
@@ -13,6 +14,14 @@ $(document).ready(function() {
 
 
 
+});
+
+
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
 });
 
 new WOW().init();
