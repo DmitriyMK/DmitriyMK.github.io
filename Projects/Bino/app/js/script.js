fae43a016155c32a0636eab16b4a7817b74b41e9
Dmitriy <alchimicus@gmail.com>
2017-03-25 19:40:52 +0200

    done bino project
    complete animation, repsonsive, burger

--- Projects/Bino/app/js/script.js
+++ Projects/Bino/app/js/script.js
@@ -1,4 +1,21 @@
+$(window).load(function() {
+	$('#preloader').find('i').fadeOut().end().delay(100).fadeOut('slow');
+});
+
 $(document).ready(function() {
+
+	/*RESPONSIVER BURGER MENU*/
+	$(".burger").click(function() {
+		$(this).toggleClass('active');
+		$(".navigation__list-item").slideToggle('fast');
+	})
+
+	$(window).resize(function() {
+		if ($(window).width() > 768) {
+			$('.navigation__list-item').removeAttr('style');
+		}
+	})
+
 
 	/*CAROUSEL*/
 	$(".owl-carousel").owlCarousel({
@@ -19,13 +36,34 @@ $(document).ready(function() {
 
 });
 
+
 /*FIXED SCROLL FOR MENU*/
 jQuery(function($) {
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > 60) {
-			$('#navigation').addClass('fixed');
-		} else if ($(this).scrollTop() < 60) {
-			$('#navigation').removeClass('fixed');
+		if ($(this).scrollTop() > 40) {
+			$('#fixedTopMenu').addClass('fixed');
+		} else if ($(this).scrollTop() < 40) {
+			$('#fixedTopMenu').removeClass('fixed');
 		}
 	});
 });
+
+/*BACK TO TOP SCROLL FOR MENU*/
+jQuery(function(f){
+	var element = f('#scrollToTop');
+	f(window).scroll(function(){
+		element['fade'+ (f(this).scrollTop() > 300 ? 'In': 'Out')](400);           
+	});
+});
+
+/*MENU SCROLLING*/
+$(document).ready(function(){
+	$('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+	});
+});
+
+new WOW().init();
