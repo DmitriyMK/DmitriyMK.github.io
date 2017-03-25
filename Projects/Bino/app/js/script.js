60e936bf0eea916228f29959ce6c92d1bc31e8a5
Dmitriy <alchimicus@gmail.com>
2017-03-25 03:29:49 +0200

    added filter and scroll-menu
    burger menu and animations in progress

--- Projects/Bino/app/js/script.js
+++ Projects/Bino/app/js/script.js
@@ -1,46 +1,31 @@
 $(document).ready(function() {
 
-  /*CAROUSEL*/
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    items: 1,
-    loop: true
-  });
-  var owl = $('.owl-carousel');
-  owl.owlCarousel();
-  $('.owl-next').click(function() {
-    owl.trigger('next.owl.carousel');
-  })
-  $('.owl-prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [700]);
-  });
+	/*CAROUSEL*/
+	$(".owl-carousel").owlCarousel({
+		center: true,
+		items: 1,
+		loop: true
+	});
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+	$('.owl-next').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+	$('.owl-prev').click(function() {
+		// With optional speed parameter
+		// Parameters has to be in square bracket '[]'
+		owl.trigger('prev.owl.carousel', [700]);
+	});
 
 });
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 400) {        
-        $('#scrollToTop').fadeIn(200);    
-    } else {
-        $('#scrollToTop').fadeOut(200);   
-    }
-});
-$('#scrollToTop').click(function() {      
-    $('body,html').animate({
-        scrollTop : 0                       
-    }, 500);
-});
-
-
-/*STICKY MENU*/
-var num = 52; //number of pixels before modifying styles
-
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
-        $('.navigation__list').addClass('fixed');
-    } else {
-        $('.navigation__list').removeClass('fixed');
-    }
+/*FIXED SCROLL FOR MENU*/
+jQuery(function($) {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 60) {
+			$('#navigation').addClass('fixed');
+		} else if ($(this).scrollTop() < 60) {
+			$('#navigation').removeClass('fixed');
+		}
+	});
 });
