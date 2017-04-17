c3c8dec350ed437ecf6915aa2cb0ed3a10e9a7ca
Dmitriy <alchimicus@gmail.com>
2017-04-17 21:11:33 +0300

    Added new projects - Photosite Elmos

---
+++ Projects/ElenaMoskvichova photography/app/js/script.js
@@ -0,0 +1,58 @@
+/*CAROUSEL*/
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  $(this).parent().css({
+    'background-image': 'url(' + $src + ')'
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function() {
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 10000,
+  pause: "false"
+});
+
+/*MENU*/
+$('#toggle').click(function() {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+});
+
+/*FILTER*/
+$(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#grid").fadeTo(100, 0.1);
+    $("#grid figure").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("." + selectedClass).fadeIn().addClass('scale-anm');
+      $("#grid").fadeTo(300, 1);
+    }, 300);
+
+  });
+});
+
+
+/*PRELOADER*/
+$(window).load(function() {
+  $('#preloader').find('i').fadeOut().end().delay(200).fadeOut('slow');
+
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+
+  });
+});
