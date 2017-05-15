587bb2c045b69b9ef9cf7eb385772da415731cbd
Dmitriy <alchimicus@gmail.com>
2017-05-16 00:39:46 +0300

    minimize img

--- Projects/ElenaMoskvichova photography/app/js/script.js
+++ Projects/ElenaMoskvichova photography/app/js/script.js
@@ -6,66 +6,67 @@ $item.height($wHeight);
 $item.addClass('full-screen');
 
 $('.carousel img').each(function() {
-  var $src = $(this).attr('src');
-  $(this).parent().css({
-    'background-image': 'url(' + $src + ')'
-  });
-  $(this).remove();
+	var $src = $(this).attr('src');
+	$(this).parent().css({
+		'background-image': 'url(' + $src + ')'
+	});
+	$(this).remove();
 });
 
 $(window).on('resize', function() {
-  $wHeight = $(window).height();
-  $item.height($wHeight);
+	$wHeight = $(window).height();
+	$item.height($wHeight);
 });
 
 $('.carousel').carousel({
-  interval: 100000,
-  pause: "false"
+	interval: 100000,
+	pause: "false"
 });
 
 
 
 /*MENU*/
 $('#toggle').click(function() {
-  $(this).toggleClass('active');
-  $('#overlay').toggleClass('open');
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
 });
 
 /*FILTER*/
 $(function() {
-    var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer',
-  });
-  var selectedClass = "";
-  $(".fil-cat").click(function() {
-    selectedClass = $(this).attr("data-rel");
-/*    $("#grid").fadeTo(100, 0.1);*/
-    $("#grid .grid-item").not("." + selectedClass).fadeOut().hide();
-    $('.grid').masonry();
-    setTimeout(function() {
-      $("." + selectedClass).fadeIn();
-      $('.grid').masonry();
-/*      $("#grid").fadeTo(100, 1);*/
-    }, 300);
+	var $grid = $('.grid').masonry({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		columnWidth: '.grid-sizer',
+	});
+	var selectedClass = "";
+	$(".fil-cat").click(function() {
+		selectedClass = $(this).attr("data-rel");
+		/*    $("#grid").fadeTo(100, 0.1);*/
+		$("#grid .grid-item").not("." + selectedClass).fadeOut().hide();
+		$('.grid').masonry();
+		setTimeout(function() {
+			$("." + selectedClass).fadeIn();
+			$('.grid').masonry();
+			/*      $("#grid").fadeTo(100, 1);*/
+		}, 300);
 
-  });
+	});
 });
 
 
 /*POPUP SLIDER-GALLERY*/
 jQuery(document).ready(function($) {
-  $('a[data-rel^=lightcase]').lightcase();
+	$('a[data-rel^=lightcase]').lightcase();
 });
 
 /*PRELOADER*/
 $(window).load(function() {
-  $('#preloader').find('i').fadeOut().end().delay(100).fadeOut('slow');
+    /* $('#preloader').find('i').fadeOut().end().delay(100).fadeOut('slow');	*/
+	$('#preloader').delay(1000).fadeOut('slow');
 
-  var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer',
-  });
+	var $grid = $('.grid').masonry({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		columnWidth: '.grid-sizer',
+	});
 });
