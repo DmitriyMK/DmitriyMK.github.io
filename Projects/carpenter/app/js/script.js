1df44f78ecfe34ec699764fc103c9b96c4c56ad3
Dmitriy <alchimicus@gmail.com>
2017-06-21 02:16:25 +0300

    Added responsive

---
+++ Projects/carpenter/app/js/script.js
@@ -0,0 +1,48 @@
+$(window).load(function() {
+	$('#preloader').find('i').fadeOut().end().delay(100).fadeOut('slow');
+});
+
+
+$(document).ready(function() {
+	/*RESPONSIVER BURGER MENU*/
+	$(".burger").click(function() {
+		$(this).toggleClass('active');
+		$(".navigation__list-item").slideToggle('fast');
+	})
+
+	$(window).resize(function() {
+		if ($(window).width() > 768) {
+			$('.navigation__list-item').removeAttr('style');
+		}
+	})
+});
+
+
+/*FIXED SCROLL FOR MENU*/
+jQuery(function($) {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 40) {
+			$('#fixedTopMenu').addClass('fixed');
+		} else if ($(this).scrollTop() < 40) {
+			$('#fixedTopMenu').removeClass('fixed');
+		}
+	});
+});
+
+/*BACK TO TOP SCROLL FOR MENU*/
+jQuery(function(f){
+	var element = f('#scrollToTop');
+	f(window).scroll(function(){
+		element['fade'+ (f(this).scrollTop() > 300 ? 'In': 'Out')](400);           
+	});
+});
+
+/*MENU SCROLLING*/
+$(document).ready(function(){
+	$('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+	});
+});
