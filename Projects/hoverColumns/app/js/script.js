3d6a1c24666683ce3a777c3be1dc301586f01acb
Dmitriy <alchimicus@gmail.com>
2017-06-29 20:12:32 +0300

    delete

--- Projects/hoverColumns/app/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	$(".burger").on("tap click", function () { 
-		$(this).toggleClass("menu-on");
-		$(this).toggleClass('active');
-		$('.nav__list').toggleClass("nav__listOn");		
-	});
-});
