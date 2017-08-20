68ab6a52a1ea61ca55e37113b1400a715983c7bc
Dmitriy <alchimicus@gmail.com>
2017-08-20 20:31:34 +0300

    Fixed portfolio items

--- Projects/MistoEnergy2/app/js/script.js
+++ Projects/MistoEnergy2/app/js/script.js
@@ -17,17 +17,7 @@ $(document).ready(function() {
 	})
 });
 
-/*MENU SCROLLING*/
-$(document).ready(function() {
-	$('a[href^="#"], *[data-href^="#"]').on('click', function(e) {
-		e.preventDefault();
-		var t = 1000;
-		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-		$('html,body').stop().animate({
-			scrollTop: $(d).offset().top
-		}, t);
-	});
-});
+
 
 $(document).ready(function() {
 $("img").lazyload({
