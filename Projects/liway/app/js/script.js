10115f7aae15c9beab3c464a4edbf744266eec1b
Dmitriy <alchimicus@gmail.com>
2017-08-28 00:15:52 +0300

    added new part

--- Projects/liway/app/js/script.js
+++ Projects/liway/app/js/script.js
@@ -61,12 +61,14 @@ $("form.callback").submit(function() {
 
 
 /*MENU SCROLLING*/
-$(document).ready(function(){
-  $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+$(document).ready(function() {
+  $('a[href^="#"], *[data-href^="#"]').on('click', function(e) {
     e.preventDefault();
     var t = 1000;
     var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-    $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+    $('html,body').stop().animate({
+      scrollTop: $(d).offset().top
+    }, t);
   });
 });
 
@@ -76,5 +78,4 @@ $(document).ready(function(){
 /*PRELOADER*/
 $(window).load(function() {
   $('#preloader').delay(100).fadeOut('slow');
-
 });
