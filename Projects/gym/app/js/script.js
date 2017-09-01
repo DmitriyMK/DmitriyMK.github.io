995b678b5c4e4170f6cf72f878cf320eabab362f
Dmitriy <alchimicus@gmail.com>
2017-09-01 03:20:13 +0300

    added new projects

---
+++ Projects/gym/app/js/script.js
@@ -0,0 +1,20 @@
+/*CAROUSEL*/
+$(document).ready(function() {
+
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+	});
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+
+	$('.owl-next').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+
+	$('.owl-prev').click(function() {
+		owl.trigger('prev.owl.carousel');
+	});
+
+});
