e019fd96760544318b8d3035d2d579d7eeb826af
Dmitriy <alchimicus@gmail.com>
2017-09-08 23:09:18 +0300

    delete caprenter project and done gym
    gym page #2 done without form elements

--- Projects/gym/app/js/script.js
+++ Projects/gym/app/js/script.js
@@ -30,7 +30,18 @@ $(document).ready(function() {
 		});
 	});
 
+	/*MENU SCROLLING*/
+	$('a[href^="#"], *[data-href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});
 });
+
+
 
 /*PRELOADER*/
 $(window).load(function() {
