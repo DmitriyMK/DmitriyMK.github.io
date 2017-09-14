36c788cd8a10c6d3426910c33075940f2d9e18c7
Dmitriy <alchimicus@gmail.com>
2017-09-15 01:00:22 +0300

    new projects

---
+++ Projects/sarto/app/js/script.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+
+	/*MENU SCROLLING*/
+	$('a[href^="#"], *[data-href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});
+
+	/*LAZY LOAD*/
+	$(function() {
+		$('.lazy').Lazy({
+			// your configuration goes here
+			scrollDirection: 'vertical',
+			effect: 'fadeIn',
+			visibleOnly: true,
+			onError: function(element) {
+				console.log('error loading ' + element.data('src'));
+			}
+		});
+	});
+
+	/*CAROUSEL*/
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		center: true,
+		items: 1,
+	});
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+
+	$('.owl-next').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+
+	$('.owl-prev').click(function() {
+		owl.trigger('prev.owl.carousel');
+	});
+
+	/*CAROUSEL #2*/
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		center: true,
+		items: 1,
+	});
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+
+	$('.owl-next2').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+
+	$('.owl-prev2').click(function() {
+		owl.trigger('prev.owl.carousel');
+	});
+
+
+});
+
+
+
+/*$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: '.trainer__link',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+});*/
+
+
+
+/*$(document).ready(function() {
+
+
+});
+*/
+
+
+
+/*PRELOADER*/
+$(window).load(function() {
+	$('#preloader').delay(100).fadeOut('slow');
+});
