c6ec2e5c572ae2e68b4e08996a37d6d424def86d
Dmitriy <alchimicus@gmail.com>
2017-09-24 19:30:27 +0300

    done

--- Projects/shoppingTour/app/js/script.js
+++ Projects/shoppingTour/app/js/script.js
@@ -11,59 +11,303 @@ $(document).ready(function() {
 	});
 
 	/*LAZY LOAD*/
-/*	$(function() {
-		$('.lazy').Lazy({
-			// your configuration goes here
-			scrollDirection: 'vertical',
-			effect: 'fadeIn',
-			visibleOnly: true,
-			onError: function(element) {
-				console.log('error loading ' + element.data('src'));
+	/*	$(function() {
+			$('.lazy').Lazy({
+				// your configuration goes here
+				scrollDirection: 'vertical',
+				effect: 'fadeIn',
+				visibleOnly: true,
+				onError: function(element) {
+					console.log('error loading ' + element.data('src'));
+				}
+			});
+		});
+		*/
+
+		/*GMAP POP-UP*/
+		$('.popup-gmaps').magnificPopup({
+			disableOn: 319,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false,
+		});
+
+		/*VIDEO POP-UP*/
+		$('.video1').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=XVFzBQTJTTM',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
 			}
 		});
-	});
-	*/
 
-	$('.popup-gmaps').magnificPopup({
-		disableOn: 319,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false,
-	});
+		$('.video2').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=r3m8cV74EJY',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
+			}
+		});
 
-	$('.publication__link, .politics__link').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#username',
-	});
+		$('.video3').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=lsMek8AOlK0',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
+			}
+		});
 
-	$(document).on('click', '.popup-modal-dismiss', function(e) {
-		e.preventDefault();
-		$.magnificPopup.close();
+		$('.video4').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=Fpw5T46NoDE',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
+			}
+		});
+
+		$('.video5').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=AEbuuKZ6SlA',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
+			}
+		});
+
+		$('.video6').magnificPopup({
+			items: {
+				src: 'https://www.youtube.com/watch?v=bZFoBnpZ6pI',
+			},
+			type: 'iframe',
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: '//www.youtube.com/embed/%id%?autoplay=1'
+					}
+				},
+				srcAction: 'iframe_src',
+			}
+		});
+
+		/*PHOTOGALLERY*/
+		/*PHOTOGALLERY*/
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>Shoping Tour</small>';
+			}
+		}
 	});
-});
+	});
 
 /*ANIMATION LET GO*/
 new WOW().init();
 
-/*EQUAL HEIGHT IN SELECT*/
-$(document).ready(function(){
 
-    var highestBox = 0;
-        $('.list').each(function(){  
-                if($(this).height() > highestBox){  
-                highestBox = $(this).height();  
-        }
-    });    
-    $('.list, .form__attention, .form-ask').height(highestBox);
 
+
+
+/*BACKGROUND VIDEO*/
+
+/*Thanks to 
+A PEN BY Vaughn D. Taylor*/
+
+// Grab data attributes from video-wrapper
+
+var videoID = $(".video-wrapper").data("video-id");
+var videoYouTubeLink = $(".video-wrapper").data("video-youtube-link");
+var videoStart = $(".video-wrapper").data("video-start");
+var videoEnd = $(".video-wrapper").data("video-end");
+var videoWidthAdd = $(".video-wrapper").data("video-width-add");
+var videoHeightAdd = $(".video-wrapper").data("video-height-add");
+
+// Prepend link to Youtube video if data attr is yes
+
+if (videoYouTubeLink === 'y') {
+	$(".video-wrapper").prepend('<a href="https://www.youtube.com/watch?v=' + videoID + '" class="video-expand" target="_blank">View on Youtube</a>');
+}
+
+// 2. This code loads the IFrame Player API code asynchronously.
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		videoId: videoID,
+		playerVars: {
+			'autoplay': 0,
+			'autohide': 1,
+			'end': videoEnd,
+			'loop': 1,
+			'modestbranding': 1,
+			'rel': 0,
+			'showinfo': 0,
+			'controls': 0,
+			'disablekb': 1,
+			'enablejsapi': 0,
+			'iv_load_policy': 3
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+// 4. The API will call this function when the video player is ready.
+
+function onPlayerReady(event) {
+	vidRescale();
+	event.target.mute();
+	event.target.seekTo(videoStart);
+}
+
+// 5. The API calls this function when the player's state changes.
+
+function onPlayerStateChange(e) {
+	if (e.data === 1) {
+		$('#player').addClass('active');
+	} else if (e.data === 0) {
+		player.seekTo(videoStart);
+	}
+}
+
+// This function scales the video to window size and uses additional values to push video beyong window size
+
+function vidRescale() {
+	console.log('video reloaded');
+	var w = $(window).width() + videoWidthAdd,
+	h = $(window).height() + videoHeightAdd;
+	if (w / h > 16 / 9) {
+		player.setSize(w, w / 16 * 9);
+		$('.tv .screen').css({
+			'left': '0px'
+		});
+	} else {
+		player.setSize(h / 9 * 16, h);
+		$('.tv .screen').css({
+			'left': -($('.tv .screen').outerWidth() - w) / 2
+		});
+	}
+}
+
+// Reloads the video on load and resize
+
+$(window).on('resize', function() {
+	vidRescale();
 });
 
 
+/*EQUAL HEIGHT IN SELECT*/
+$(document).ready(function() {
 
+	var highestBox = 0;
+	$('.list').each(function() {
+		if ($(this).height() > highestBox) {
+			highestBox = $(this).height();
+		}
+	});
+	$('.list, .form__attention, .form-ask').height(highestBox);
 
+});
+
+/*CHANGE COLUMN`S COLOR*/
+
+$(document).ready(function() {
+    $('.form__item').click(function() {
+        $('.form__item').removeClass('active');
+        $(this).addClass('active');
+    });
+});
 
 
 /*PRELOADER*/
