86363d189b98d856540ddbc27a6894dc5f4dac9b
Dmitriy <alchimicus@gmail.com>
2017-10-17 00:41:12 +0300

    fixed styles
    done

--- Projects/famillyPhoto/app/js/script.js
+++ Projects/famillyPhoto/app/js/script.js
@@ -1,79 +1,76 @@
-$(function() {
-	$('.header__link').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#name',
+$(document).ready(function() {
+
+	$(function() {
+		$('.header__link').magnificPopup({
+			type: 'inline',
+			preloader: false,
+			focus: '#name',
+		});
+		$(document).on('click', '.popup-modal-dismiss', function(e) {
+			e.preventDefault();
+			$.magnificPopup.close();
+		});
 	});
-	$(document).on('click', '.popup-modal-dismiss', function(e) {
-		e.preventDefault();
-		$.magnificPopup.close();
+
+	$(function() {
+		$('.video__link').magnificPopup({
+			type: 'inline',
+			preloader: false,
+			focus: '#name',
+		});
+		$(document).on('click', '.popup-modal-dismiss', function(e) {
+			e.preventDefault();
+			$.magnificPopup.close();
+		});
 	});
-});
 
-$(function() {
-	$('.video__link').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#name',
+	/*CAROUSEL*/
+	$(".owl-carousel").owlCarousel({
+		center: true,
+		loop: true,
+		items: 1,
+		smartSpeed: 400
 	});
-	$(document).on('click', '.popup-modal-dismiss', function(e) {
-		e.preventDefault();
-		$.magnificPopup.close();
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+
+	$('.owl-next').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+
+	$('.owl-prev').click(function() {
+		owl.trigger('prev.owl.carousel');
 	});
-});
 
-/*CAROUSEL*/
-$(".owl-carousel").owlCarousel({
-	center: true,
-	loop: true,
-	items: 1,
-	smartSpeed: 400
-});
-
-var owl = $('.owl-carousel');
-owl.owlCarousel();
-
-$('.owl-next').click(function() {
-	owl.trigger('next.owl.carousel');
-})
-
-$('.owl-prev').click(function() {
-	owl.trigger('prev.owl.carousel');
-});
-
-/*PHOTOGALLERY*/
-$('.popup-gallery').magnificPopup({
-	delegate: 'a',
-	type: 'image',
-	tLoading: 'Loading image #%curr%...',
-	mainClass: 'mfp-img-mobile',
-	gallery: {
-		enabled: true,
-		navigateByImgClick: true,
-		preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-	},
-	image: {
-		tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-		titleSrc: function(item) {
-			return item.el.attr('title') + '<small></small>';
+	/*PHOTOGALLERY*/
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small></small>';
+			}
 		}
-	}
-});
+	});
 
-$('.grid').masonry({
-	temSelector: '.grid-item',
-	percentPosition: true,
-	columnWidth: '.grid-sizer',
-});
+	$('.grid').masonry({
+		temSelector: '.grid-item',
+		percentPosition: true,
+		columnWidth: '.grid-sizer',
+	});
 
+});
 
 /*PRELOADER*/
 $(window).load(function() {
 	$('#preloader').delay(100).fadeOut('slow');
-
-	$('.grid').masonry({
-		itemSelector: '.grid-item',
-		percentPosition: true,
-		columnWidth: '.grid-sizer',
-	});
 });
