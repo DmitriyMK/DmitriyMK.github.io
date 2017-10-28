91bbf36222aa01ef1d8affbdede8a263ab95d4f3
Dmitriy <alchimicus@gmail.com>
2017-10-28 18:12:17 +0300

    added new projects

---
+++ Projects/Mining/app/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+	/*MENU SCROLLING*/
+	$('.scroll__link').on('click', function(e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});
+
+
+	$(function(){
+		var $gallery = $('.gallery a').simpleLightbox();
+
+	});
+
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+	$('.politics__link').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+	});
+	
+});
+
+
+
+/*PRELOADER*/
+$(window).load(function() {
+	$('#preloader').delay(100).fadeOut('slow');
+});
