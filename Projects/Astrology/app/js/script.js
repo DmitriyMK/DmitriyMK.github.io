eabd0d2499c5ae94cdc5ff10a0a67cf27096086a
Dmitriy <alchimicus@gmail.com>
2017-11-18 00:08:09 +0200

    Added new styles to Astro

---
+++ Projects/Astrology/app/js/script.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+
+	/*MENU SCROLLING*/
+	$('.scroll__link').on('click', function(e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});
+
+	/*RESPONSIVER BURGER MENU*/
+	$(".burger").click(function() {
+		$(this).toggleClass('active');
+		$(".nav__list").slideToggle('fast');
+	})
+
+	$('.burger').on('click', function() {
+		$('.overlay').toggleClass('open').show;
+	});
+
+	$(window).resize(function() {
+		if ($(window).width() > 768) {
+			$('.nav__list').removeAttr('style');
+		}
+	})
+
+	/*LAZY LOAD*/
+	$(function() {
+		$('.lazy').Lazy({
+			// your configuration goes here
+			scrollDirection: 'vertical',
+			effect: 'fadeIn',
+			visibleOnly: true,
+			onError: function(element) {
+				console.log('error loading ' + element.data('src'));
+			}
+		});
+	});
+
+
+	/*	$(function() {
+			var $gallery = $('.galleryl a').simpleLightbox();
+
+		});*/
+
+
+	$('.review__slides').slick({
+		infinite: true,
+		speed: 500,
+	});
+
+
+
+	/*GMAP POP-UP*/
+	/*	$('.popup-gmaps').magnificPopup({
+			disableOn: 319,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false,
+		});*/
+
+	/*FORM POP-UP*/
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		callbacks: {
+			beforeOpen: function() {
+				if ($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+	$('.politics__link').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+	});
+
+
+	/*	new WOW().init();*/
+
+	jQuery('.tipso').tipso({
+		background: '#fff',
+		color: '#000',
+		width: '250px',
+		position: 'top',
+	});
+
+});
+
+$(function() {
+	$('.select-line').selectize({});
+});
+
+
+$(function() {
+	$(".articles").slice(0, 1).show();
+	$("#loadMore").on('click', function(e) {
+		e.preventDefault();
+		$(".articles:hidden").slice(0, 2).slideDown();
+		if ($(".articles:hidden").length == 0) {
+			$("#loadMore").fadeOut('slow');
+		}
+		$('html,body').animate({
+			scrollTop: $(this).offset().top
+		}, 1500);
+		if ($(".articles:last").css('display') == 'block') {
+			$('#loadMore').hide();
+			/*$('.totop').show()*/
+		}
+	});
+});
+
+
+
+//E-mail Ajax Send
+$("form.open-popup-link").submit(function() {
+	var th = $(this);
+	$.ajax({
+		type: "POST",
+		url: "../js/mail.php",
+		data: th.serialize()
+	}).done(function() {
+		$(th).find('.success').addClass('active').css("display", "flex").hide().fadeIn();
+		setTimeout(function() {
+			$(th).find('.success').removeClass('active').fadeOut();
+			th.trigger("reset");
+		}, 3000);
+	});
+	return false;
+});
