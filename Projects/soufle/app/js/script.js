72da2c8a19ebf6e9a3918d315c6633d0d6b790d9
Dmitriy <alchimicus@gmail.com>
2017-12-06 18:29:29 +0200

    added new pro

---
+++ Projects/soufle/app/js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+	/*MENU SCROLLING*/
+/*	$('.scroll__link').on('click', function(e) {
+		e.preventDefault();
+		var t = 1000;
+		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+		$('html,body').stop().animate({
+			scrollTop: $(d).offset().top
+		}, t);
+	});*/
+
+
+/*	$(function() {
+		var $gallery = $('.galleryl a').simpleLightbox();
+
+	});*/
+
+
+	$('.sliderText').slick({
+		arrows: true,
+		infinite: true,
+		slidesToShow: 1,
+		speed: 500,
+	});
+
+
+
+	/*GMAP POP-UP*/
+/*	$('.popup-gmaps').magnificPopup({
+		disableOn: 319,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+	});*/
+	
+	/*FORM POP-UP*/
+/*	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		callbacks: {
+			beforeOpen: function() {
+				if ($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});*/
+
+/*	$('.politics__link').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+	});*/
+
+
+/*	new WOW().init();*/
+
+});
+
+
+
+//E-mail Ajax Send
+/*$("form.open-popup-link").submit(function() {
+	var th = $(this);
+	$.ajax({
+		type: "POST",
+		url: "../js/mail.php",
+		data: th.serialize()
+	}).done(function() {
+		$(th).find('.success').addClass('active').css("display", "flex").hide().fadeIn();
+		setTimeout(function() {
+			$(th).find('.success').removeClass('active').fadeOut();
+			th.trigger("reset");
+		}, 3000);
+	});
+	return false;
+});
+*/
+/*PRELOADER*/
+$(window).load(function() {
+	$('#preloader').delay(100).fadeOut('slow');
+
+});
