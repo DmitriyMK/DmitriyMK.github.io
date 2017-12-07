75c4731aa7e54d189334f58a51d145243dc4f6f7
Dmitriy <alchimicus@gmail.com>
2017-12-08 00:45:29 +0200

    added new section

--- Projects/soufle/app/js/script.js
+++ Projects/soufle/app/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 	/*MENU SCROLLING*/
-/*	$('.scroll__link').on('click', function(e) {
+	$('.scroll__link').on('click', function(e) {
 		e.preventDefault();
 		var t = 1000;
 		var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
 		$('html,body').stop().animate({
 			scrollTop: $(d).offset().top
 		}, t);
-	});*/
+	});
 
 
 /*	$(function() {
