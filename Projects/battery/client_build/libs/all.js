4fe34d3b244750d82203ebe3e8f01712ce5c4a9a
DmitriyMK <alchimic@ua.fm>
2018-04-13 12:22:34 +0300

    added percentage animation

--- Projects/battery/client_build/libs/all.js
+++ Projects/battery/client_build/libs/all.js
@@ -107,6 +107,22 @@ $(document).ready(function() {
   });
 
 
+
+ $('.bar-percentage[data-percentage]').each(function () {
+  var progress = $(this);
+  var percentage = Math.ceil($(this).attr('data-percentage'));
+  $({countNum: 0}).animate({countNum: percentage}, {
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      // What todo on every count
+      var pct = Math.floor(this.countNum) + '%';
+      progress.text(pct) && progress.siblings().children().css('width',pct);
+    }
+  });
+});
+
+
 /*  $('.slider-nav').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
