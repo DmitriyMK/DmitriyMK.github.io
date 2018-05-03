3ebcd72490e87d26f414dbc01971cfd6d0d92a91
DmitriyMK <alchimic@ua.fm>
2018-05-03 12:39:53 +0300

    fix js

--- Projects/road/client_build/libs/all-news.js
+++ Projects/road/client_build/libs/all-news.js
@@ -41,44 +41,57 @@ $(document).ready(function() {
       });
     }
   });
+
 });
-
-
-/*RESPONSIVER BURGER MENU*/
-var $menu = $('.nav');
-
-$('.burger').click(function() {
-  $(this).toggleClass('active');
-  $('.overlay').toggleClass('open').show;
-});
-
-
-$(document).mouseup(function(e) {
-  if (!$menu.is(e.target) // if the target of the click isn't the container...
-    &&
-    $menu.has(e.target).length === 0) // ... nor a descendant of the container
-  {
-
-    $('.overlay').removeClass('open');
-    $(".burger").removeClass('active');
-  }
-});
-
 
 
 $(document).ready(function() {
+
+  /*RESPONSIVER BURGER MENU*/
+
+  var $menu = $('.menu');
+
+  $('.burger').click(function () {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('open').show;
+    /*$menu.toggle();*/
+  });
+
+  $(document).mouseup(function (e) {
+   if (!$menu.is(e.target) // if the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   {
+     /*$menu.hide();*/
+     $('.overlay').removeClass('open');
+     $(".burger").removeClass('active');
+   }
+
+   $('.burger').click(function () {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('open').show;
+    /*$menu.toggle();*/
+  });
+
+ });
+  
+
+
 
   $(".linkForSubMenu1").click(function() {
 
     $(this).toggleClass('active');
     $(".nav__list-submenu").slideToggle(300);
+
   });
 
   $(".linkForSubMenu2").click(function() {
 
     $(this).toggleClass('active');
     $(".nav__list-submenu2").slideToggle(300);
+
   });
+
+
 
   $('.scrolling__link').on('click', function(e) {
     e.preventDefault();
@@ -89,4 +102,28 @@ $(document).ready(function() {
     }, t);
   });
 
+
+
 });
+
+
+
+
+
+// $(".form").submit(function() {
+//   var th = $(this);
+//   $.ajax({
+//     type: "POST",
+//     url: "mail.php",
+//     data: th.serialize()
+//   }).done(function() {
+
+//     var inst = $('[data-remodal-id=modal-thanks]').remodal();
+//     inst.open();
+
+//     setTimeout(function() {
+//       th.trigger("reset");
+//     }, 1000);
+//   });
+//   return false;
+// });
