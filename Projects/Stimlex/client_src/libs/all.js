3c013e5260418c6b9ab073d4cf3b52a25773cf45
DmitriyMK <alchimic@ua.fm>
2018-05-14 18:14:01 +0300

    add section review, contact, footer

--- Projects/Stimlex/client_src/libs/all.js
+++ Projects/Stimlex/client_src/libs/all.js
@@ -206,15 +206,16 @@ $(".form").submit(function() {
 
 
 
-/*function initMap() {
 
-  var latlng = new google.maps.LatLng(50.45843182, 30.40665988);
+function initMap() {
+
+  var latlng = new google.maps.LatLng(48.4410735, 35.0167546);
   var settings = {
-    zoom: 15,
+    zoom: 17,
     center: latlng,
     center: {
-      lat: 50.45843182,
-      lng: 30.40665988
+      lat: 48.4410735,
+      lng: 35.0187546
     },
     mapTypeControl: true,
     mapTypeControlOptions: {
@@ -226,118 +227,13 @@ $(".form").submit(function() {
       style: google.maps.NavigationControlStyle.SMALL
     },
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles:
+/*    styles:*/
 
 
-      [{
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#f5f5f5"
-      }]
-    }, {
-      "elementType": "labels.icon",
-      "stylers": [{
-        "visibility": "off"
-      }]
-    }, {
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#616161"
-      }]
-    }, {
-      "elementType": "labels.text.stroke",
-      "stylers": [{
-        "color": "#f5f5f5"
-      }]
-    }, {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#bdbdbd"
-      }]
-    }, {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#eeeeee"
-      }]
-    }, {
-      "featureType": "poi",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#757575"
-      }]
-    }, {
-      "featureType": "poi.park",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#e5e5e5"
-      }]
-    }, {
-      "featureType": "poi.park",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#9e9e9e"
-      }]
-    }, {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#ffffff"
-      }]
-    }, {
-      "featureType": "road.arterial",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#757575"
-      }]
-    }, {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#dadada"
-      }]
-    }, {
-      "featureType": "road.highway",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#616161"
-      }]
-    }, {
-      "featureType": "road.local",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#9e9e9e"
-      }]
-    }, {
-      "featureType": "transit.line",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#e5e5e5"
-      }]
-    }, {
-      "featureType": "transit.station",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#eeeeee"
-      }]
-    }, {
-      "featureType": "water",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#c9c9c9"
-      }]
-    }, {
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#9e9e9e"
-      }]
-    }]
-
+      
   };
   var map = new google.maps.Map(document.getElementById("google-map"), settings);
-  var myLatlng = new google.maps.LatLng(50.45843182, 30.40665988);
+  var myLatlng = new google.maps.LatLng(48.4410735, 35.0167546);
 
   var myOptions = {
     zoom: 10,
@@ -346,15 +242,15 @@ $(".form").submit(function() {
     disableDoubleClickZoom: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
-  var image = 'images/mark.png';
+  var image = 'images/icons/marker.png';
   var beachMarker = new google.maps.Marker({
     position: {
-      lat: 50.45843182,
-      lng: 30.40665988
+      lat: 48.4410735,
+      lng: 35.0167546
     },
     map: map,
     icon: image
       // title: 'Push-k Solutions'
   });
 
-};*/
+};
