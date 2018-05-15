a1e6382a5a23d141e48bf98fbc70ec02c95e91f3
DmitriyMK <alchimic@ua.fm>
2018-05-15 19:11:04 +0300

    add function

--- Projects/Stimlex/client_build/libs/all.js
+++ Projects/Stimlex/client_build/libs/all.js
@@ -182,11 +182,42 @@ $(document).ready(function() {
   /*SWITCH BARBECUE GALLERY*/
 
   $("#crasia").bind("click", function() {
+
     $(".construction img").removeClass("borderActive");
     $(".click__imgCrasia").addClass("borderActive");
 
-    $(".big__item").removeClass("active");
-    $("#CrasiaBLF").addClass("active");
+    $("#labradorit").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgLabr").addClass("borderActive");
+
+      $("#tabletopType").text("«Лабрадорит»");
+
+      $(".big__item").removeClass("active");
+      $("#CrasiaBLF").addClass("active");
+    });
+
+
+    $("#megerichka").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgMeger").addClass("borderActive");
+
+      $("#tabletopType").text("«Межеричка»");
+
+      $(".big__item").removeClass("active");
+      $("#CrasiaBMF").addClass("active");
+    });
+
+
+    $("#pokostovskiy").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgPokost").addClass("borderActive");
+
+      $("#tabletopType").text("«Покостовский»");
+
+      $(".big__item").removeClass("active");
+      $("#CrasiaBPF").addClass("active");
+    });
+
   });
 
 
@@ -194,42 +225,41 @@ $(document).ready(function() {
     $(".construction img").removeClass("borderActive");
     $(".click__imgSteel").addClass("borderActive");
 
-    $(".big__item").removeClass("active");
-    $("#CrasiaBMF").addClass("active");
+
+    $("#labradorit").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgLabr").addClass("borderActive");
+
+      $("#tabletopType").text("«Лабрадорит»");
+
+      $(".big__item").removeClass("active");
+      $("#SteelBLF").addClass("active");
+    });
+
+
+    $("#megerichka").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgMeger").addClass("borderActive");
+
+      $("#tabletopType").text("«Межеричка»");
+
+      $(".big__item").removeClass("active");
+      $("#SteelBMF").addClass("active");
+    });
+
+
+    $("#pokostovskiy").bind("click", function() {
+      $(".tabletop img").removeClass("borderActive");
+      $(".click__imgPokost").addClass("borderActive");
+
+      $("#tabletopType").text("«Покостовский»");
+
+      $(".big__item").removeClass("active");
+      $("#SteelBPF").addClass("active");
+    });
+
   });
 
-
-  $("#labradorit").bind("click", function() {
-    $(".tabletop img").removeClass("borderActive");
-    $(".click__imgLabr").addClass("borderActive");
-
-    $("#tabletopType").text("«Лабрадорит»");
-
-    $(".big__item").removeClass("active");
-    $("#SteelBLF").addClass("active");
-  });
-
-
-  $("#megerichka").bind("click", function() {
-    $(".tabletop img").removeClass("borderActive");
-    $(".click__imgMeger").addClass("borderActive");
-
-    $("#tabletopType").text("«Межеричка»");
-
-    $(".big__item").removeClass("active");
-    $("#SteelBMF").addClass("active");
-  });
-
-
-  $("#pokostovskiy").bind("click", function() {
-    $(".tabletop img").removeClass("borderActive");
-    $(".click__imgPokost").addClass("borderActive");
-
-    $("#tabletopType").text("«Покостовский»");
-
-    $(".big__item").removeClass("active");
-    $("#SteelBPF").addClass("active");
-  });
 
 
 });
