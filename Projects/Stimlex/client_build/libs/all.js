1ded92dfcf181c0f78334a46bffb041496d4a91d
DmitriyMK <alchimic@ua.fm>
2018-05-16 18:37:03 +0300

    fix

--- Projects/Stimlex/client_build/libs/all.js
+++ Projects/Stimlex/client_build/libs/all.js
@@ -42,6 +42,7 @@ $(document).ready(function() {
 });*/
 
 
+
 $(document).ready(function() {
 
   /*  if (document.documentElement.clientWidth > 1200) {
@@ -51,96 +52,85 @@ $(document).ready(function() {
 
     */
 
-  $('.scrolling__link').on('click', function(e) {
-    e.preventDefault();
-    var t = 1500;
-    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-    $('html,body').stop().animate({
-      scrollTop: $(d).offset().top
-    }, t);
-  });
+    if (document.documentElement.clientWidth > 1200) {
+      $(window).enllax();
 
-  $(".js-video-button").modalVideo({
-    youtube: {
-      autoplay: 1,
-      nocookie: true,
-      cc_load_policy: 1,
-      color: null,
-      controls: 1,
-      disablekb: 0,
-      enablejsapi: 0,
-      end: null,
-      fs: 1,
-      h1: null,
-      iv_load_policy: 1,
-      list: null,
-      listType: null,
-      loop: 0,
-      modestbranding: null,
-      origin: null,
-      playlist: null,
-      playsinline: null,
-      rel: 0,
-      showinfo: 1,
-      start: 0,
-      wmode: 'transparent',
-      theme: 'dark'
+    };
 
-    }
-  });
-
-
-
-  /*LAZY LOAD*/
-  /*  $('.lazy').lazy({
-      effect: "fadeIn",
-      effectTime: 200,
-      threshold: 0,
-      scrollDirection: 'vertical',
-      visibleOnly: true,
-
-      onError: function(element) {
-        console.log('error loading ' + element.data('src'));
-      }
-
+    $('.scrolling__link').on('click', function(e) {
+      e.preventDefault();
+      var t = 1500;
+      var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+      $('html,body').stop().animate({
+        scrollTop: $(d).offset().top
+      }, t);
     });
-    */
 
-  $('.slider__barbecue').slick({
-    slidesToShow: 5,
-    arrows: false,
-    dots: false,
-  });
+    $(".js-video-button").modalVideo({
+      youtube: {
+        autoplay: 1,
+        nocookie: true,
+        cc_load_policy: 1,
+        color: null,
+        controls: 1,
+        disablekb: 0,
+        enablejsapi: 0,
+        end: null,
+        fs: 1,
+        h1: null,
+        iv_load_policy: 1,
+        list: null,
+        listType: null,
+        loop: 0,
+        modestbranding: null,
+        origin: null,
+        playlist: null,
+        playsinline: null,
+        rel: 0,
+        showinfo: 1,
+        start: 0,
+        wmode: 'transparent',
+        theme: 'dark'
+
+      }
+    });
 
 
-  $('.slider__product').slick({
-    slidesToShow: 5,
-    arrows: false,
-    dots: false,
-  });
+    $('.slider__barbecue').slick({
+      slidesToShow: 5,
+      arrows: false,
+      dots: false,
+    });
+
+
+    $('.slider__product').slick({
+      slidesToShow: 5,
+      arrows: false,
+      dots: false,
+    });
 
 
 
-  $('.slider-for-recipe').slick({
-    slidesToShow: 1,
-    arrows: false,
-    dots: false,
-    asNavFor: '.slider-nav-recipe',
-    fade: true,
-    draggable: false,
-    infinite: true,
-  });
+    $('.slider-for-recipe').slick({
+      slidesToShow: 1,
+      arrows: false,
+      dots: false,
+      asNavFor: '.slider-nav-recipe',
+      fade: true,
+      draggable: false,
+      infinite: true,
+    });
 
 
-  $('.slider-nav-recipe').slick({
-    slidesToShow: 4,
-    asNavFor: '.slider-for-recipe',
-    arrows: false,
-    dots: false,
-    focusOnSelect: true,
-    infinite: true,
+    $('.slider-nav-recipe').slick({
+      slidesToShow: 4,
+      asNavFor: '.slider-for-recipe',
+      arrows: false,
+      dots: false,
+      focusOnSelect: true,
+      infinite: true,
 
-    responsive: [
+      responsive: [
 
       /*    {
             breakpoint: 1024,
@@ -163,30 +153,29 @@ $(document).ready(function() {
             }
           }*/
 
-    ]
+          ]
+        });
+
+    $('.slider-review').slick({
+      slidesToShow: 1,
+      draggable: false,
+      infinite: true,
+      dots: true,
+      fade: true,
+    });
+
+
+
+    $('[data-fancybox]').fancybox({
+      loop: false,
+      animationEffect: "zoom",
+      transitionEffect: "fade",
+      transitionDuration: 366,
+
+      clickOutside: "close",
+    });
+
   });
-
-  $('.slider-review').slick({
-    slidesToShow: 1,
-    draggable: false,
-    infinite: true,
-    dots: true,
-    fade: true,
-  });
-
-
-
-  $('[data-fancybox]').fancybox({
-    loop: false,
-    animationEffect: "zoom",
-    transitionEffect: "fade",
-    transitionDuration: 366,
-
-    clickOutside: "close",
-  });
-
-});
-
 
 
 
@@ -223,39 +212,39 @@ $(".labradorit").bind("click", function() {
   $(".tabletop img").removeClass("borderActive");
   $(".click__imgLabr").addClass("borderActive");
 
-$(".labradorit").bind("click", function() {
-  $(".tabletop img").removeClass("borderActive");
-  $(".click__imgLabr").addClass("borderActive"); //Рамочки
+  $(".labradorit").bind("click", function() {
+    $(".tabletop img").removeClass("borderActive");
+    $(".click__imgLabr").addClass("borderActive"); //Рамочки
 
-  $(".tabletopType").text("«Лабрадорит»"); // Изменение текста в описании
+    $(".tabletopType").text("«Лабрадорит»"); // Изменение текста в описании
 
-  $(".big__item").removeClass("active");
-  $("#CrasiaBLF").addClass("active");  //Изначально display: none. при добавлении класа active, display: block;
-});
-
-
-$(".megerichka").bind("click", function() {
-  $(".tabletop img").removeClass("borderActive");
-  $(".click__imgMeger").addClass("borderActive");
-
-  $(".tabletopType").text("«Межеричка»");
-
-  $(".tabletopType").text("«Лабрадорит»");
-
-  $(".big__item").removeClass("active");
-  $("#CrasiaBLF").addClass("active");
-});
+    $(".big__item").removeClass("active");
+    $("#CrasiaBLF").addClass("active"); //Изначально display: none. при добавлении класа active, display: block;
+  });
 
 
-$(".megerichka").bind("click", function() {
-  $(".tabletop img").removeClass("borderActive");
-  $(".click__imgMeger").addClass("borderActive");
+  $(".megerichka").bind("click", function() {
+    $(".tabletop img").removeClass("borderActive");
+    $(".click__imgMeger").addClass("borderActive");
 
-  $(".tabletopType").text("«Межеричка»");
+    $(".tabletopType").text("«Межеричка»");
 
-  $(".big__item").removeClass("active");
-  $("#CrasiaBMF").addClass("active");
-});
+    $(".tabletopType").text("«Лабрадорит»");
+
+    $(".big__item").removeClass("active");
+    $("#CrasiaBLF").addClass("active");
+  });
+
+
+  $(".megerichka").bind("click", function() {
+    $(".tabletop img").removeClass("borderActive");
+    $(".click__imgMeger").addClass("borderActive");
+
+    $(".tabletopType").text("«Межеричка»");
+
+    $(".big__item").removeClass("active");
+    $("#CrasiaBMF").addClass("active");
+  });
 
 
   $(".construction img").removeClass("borderActive");
@@ -376,76 +365,76 @@ $(".pokostovskiy").bind("click", function() {
 
 
 
-$(window).on('resize orientationchange', function() {
-  $('.slider').slick('resize');
-});
-
-
-
-$(".form").submit(function() {
-  var th = $(this);
-  $.ajax({
-    type: "POST",
-    url: "mail.php",
-    data: th.serialize()
-  }).done(function() {
-
-    var inst = $('[data-remodal-id=modal-thanks]').remodal();
-    inst.open();
-
-    setTimeout(function() {
-      th.trigger("reset");
-    }, 1000);
+  $(window).on('resize orientationchange', function() {
+    $('.slider').slick('resize');
   });
-  return false;
-});
 
 
 
-function initMap() {
+  $(".form").submit(function() {
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: th.serialize()
+    }).done(function() {
 
-  var latlng = new google.maps.LatLng(48.4410735, 35.0167546);
-  var settings = {
-    zoom: 17,
-    center: latlng,
-    center: {
-      lat: 48.4410735,
-      lng: 35.0187546
-    },
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-    },
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    navigationControlOptions: {
-      style: google.maps.NavigationControlStyle.SMALL
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    /*    styles:*/
+      var inst = $('[data-remodal-id=modal-thanks]').remodal();
+      inst.open();
+
+      setTimeout(function() {
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 
 
+
+  function initMap() {
+
+    var latlng = new google.maps.LatLng(48.4410735, 35.0167546);
+    var settings = {
+      zoom: 17,
+      center: latlng,
+      center: {
+        lat: 48.4410735,
+        lng: 35.0187546
+      },
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      },
+      scrollwheel: false,
+      disableDoubleClickZoom: true,
+      navigationControlOptions: {
+        style: google.maps.NavigationControlStyle.SMALL
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      /*    styles:*/
+
+
+
+    };
+    var map = new google.maps.Map(document.getElementById("google-map"), settings);
+    var myLatlng = new google.maps.LatLng(48.4410735, 35.0167546);
+
+    var myOptions = {
+      zoom: 10,
+      center: myLatlng,
+      scrollwheel: false,
+      disableDoubleClickZoom: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var image = 'images/icons/marker.png';
+    var beachMarker = new google.maps.Marker({
+      position: {
+        lat: 48.4410735,
+        lng: 35.0167546
+      },
+      map: map,
+      icon: image
+      // title: 'Push-k Solutions'
+    });
 
   };
-  var map = new google.maps.Map(document.getElementById("google-map"), settings);
-  var myLatlng = new google.maps.LatLng(48.4410735, 35.0167546);
-
-  var myOptions = {
-    zoom: 10,
-    center: myLatlng,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var image = 'images/icons/marker.png';
-  var beachMarker = new google.maps.Marker({
-    position: {
-      lat: 48.4410735,
-      lng: 35.0167546
-    },
-    map: map,
-    icon: image
-    // title: 'Push-k Solutions'
-  });
-
-};
