7d718bf0debb9ae6e880d9eaf1f5f501d40516f3
DmitriyMK <alchimic@ua.fm>
2018-05-24 17:21:59 +0300

    add picture

---
+++ Projects/powerbank/client_build/libs/all.js
@@ -0,0 +1,116 @@
+
+$(document).ready(function() {
+
+
+  if (document.documentElement.clientWidth > 1200) {
+
+/*    new WOW().init();*/
+
+    $("body").niceScroll({
+      scrollspeed: 70,
+      mousescrollstep: 60,
+      smoothscroll: true,
+      cursorwidth: 8,
+      cursorborder: 0,
+      cursorcolor: '#fe4980',
+      cursorborderradius: 2,
+      autohidemode: true,
+      horizrailenabled: false,
+      cursoropacitymin: 1,
+      background: false,
+      zindex: "9999"
+    });
+
+    $('html').addClass('no-overflow-y');
+
+  };
+
+
+  $('.scrolling__link').on('click', function(e) {
+    e.preventDefault();
+    var t = 1500;
+    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+    $('html,body').stop().animate({
+      scrollTop: $(d).offset().top
+    }, t);
+  });
+
+
+
+/*  $('.slider-for-recipe').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    asNavFor: '.slider-nav-recipe',
+    fade: true,
+    draggable: false,
+    infinite: true,
+
+  });*/
+
+
+/*  $('.slider-nav-recipe').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for-recipe',
+    arrows: false,
+    dots: false,
+    focusOnSelect: true,
+    infinite: true,
+    centerMode: false,
+
+    responsive: [
+
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+
+      }
+    },
+
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        arrows: true,
+      }
+    },
+
+    ]
+
+  });
+*/
+
+});
+
+
+/*
+$(window).on('resize orientationchange', function() {
+  $('.slider').slick('resize');
+});
+*/
+
+
+$(".form").submit(function() {
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "mail.php",
+    data: th.serialize()
+  }).done(function() {
+
+    var inst = $('[data-remodal-id=modal-thanks]').remodal();
+    inst.open();
+
+    setTimeout(function() {
+      th.trigger("reset");
+    }, 1000);
+  });
+  return false;
+});
