f61a60389b2362e210dabcc3291d5e38baeb5a4b
DmitriyMK <alchimic@ua.fm>
2018-06-05 16:30:16 +0300

    fix salons

--- Projects/salon/client_build/libs/all.js
+++ Projects/salon/client_build/libs/all.js
@@ -1,110 +1,110 @@
 $(document).ready(function() {
 
 
-  if (document.documentElement.clientWidth > 1200) {
+      if (document.documentElement.clientWidth > 1200) {
 
-    /*   new WOW().init(); */
+        /*   new WOW().init(); */
 
-    $("body").niceScroll({
-      scrollspeed: 70,
-      mousescrollstep: 60,
-      smoothscroll: true,
-      cursorwidth: 8,
-      cursorborder: 0,
-      cursorcolor: '#feaf00',
-      cursorborderradius: 2,
-      autohidemode: true,
-      horizrailenabled: false,
-      cursoropacitymin: 1,
-      background: false,
-      zindex: "9999"
+        $("body").niceScroll({
+          scrollspeed: 70,
+          mousescrollstep: 60,
+          smoothscroll: true,
+          cursorwidth: 8,
+          cursorborder: 0,
+          cursorcolor: '#feaf00',
+          cursorborderradius: 2,
+          autohidemode: true,
+          horizrailenabled: false,
+          cursoropacitymin: 1,
+          background: false,
+          zindex: "9999"
+        });
+
+        $('html').addClass('no-overflow-y');
+
+      };
+
     });
 
-    $('html').addClass('no-overflow-y');
 
-  };
+    $(document).ready(function() {
 
-
-  $('.scrolling__link').on('click', function(e) {
-    e.preventDefault();
-    var t = 1500;
-    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-    $('html,body').stop().animate({
-      scrollTop: $(d).offset().top
-    }, t);
-  });
+      $('.scrolling__link').on('click', function(e) {
+        e.preventDefault();
+        var t = 1500;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({
+          scrollTop: $(d).offset().top
+        }, t);
+      });
 
 
-
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
+      $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+      });
 
 
-  $('.phone-mask').mask('+38(999)999-99-99');
+      $('.phone-mask').mask('+38(999)999-99-99');
 
 
-  $('.slider__present').slick({
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    infinite: true,
-
-  });
-
-
-  $('.office__slider').slick({
-    arrows: true,
-    dots: false,
-
-  });
+      $('.slider__present').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        infinite: true,
+      });
 
 
-  $('.review__slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-  });
+      $('.office__slider').slick({
+        arrows: true,
+        dots: false,
+      });
 
 
-  $('[data-fancybox]').fancybox({
-    loop: true,
-    animationEffect: "zoom",
-    transitionEffect: "fade",
-    transitionDuration: 366,
+      $('.review__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+      });
 
-    clickOutside: "close",
-  });
-  
 
-});
+      $('[data-fancybox]').fancybox({
+        loop: true,
+        animationEffect: "zoom",
+        transitionEffect: "fade",
+        transitionDuration: 366,
+
+        clickOutside: "close",
+      });
+
+
+    });
 
 
 
-$(window).on('resize orientationchange', function() {
-  $('.slider').slick('resize');
-});
+    $(window).on('resize orientationchange', function() {
+      $('.slider').slick('resize');
+    });
 
 
 
-$(".form").submit(function() {
-  var th = $(this);
-  $.ajax({
-    type: "POST",
-    url: "mail.php",
-    data: th.serialize()
-  }).done(function() {
+    $(".form").submit(function() {
+      var th = $(this);
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: th.serialize()
+      }).done(function() {
 
-    var inst = $('[data-remodal-id=modal-thanks]').remodal();
-    inst.open();
+        var inst = $('[data-remodal-id=modal-thanks]').remodal();
+        inst.open();
 
-    setTimeout(function() {
-      th.trigger("reset");
-    }, 1000);
-  });
-  return false;
-});
+        setTimeout(function() {
+          th.trigger("reset");
+        }, 1000);
+      });
+      return false;
+    });
