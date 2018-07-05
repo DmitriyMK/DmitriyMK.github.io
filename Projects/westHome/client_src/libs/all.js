9a34dca544fbb420f4dfcc427aa2094208e73c08
DmitriyMK <alchimic@ua.fm>
2018-07-05 17:28:32 +0300

    add new section

--- Projects/westHome/client_src/libs/all.js
+++ Projects/westHome/client_src/libs/all.js
@@ -51,88 +51,28 @@ $(document).ready(function() {
   $('.phone-mask').mask('+38(999)999-99-99');
 
 
-  /*  $(".js-video-button").modalVideo({
-      youtube: {
-        autoplay: 1,
-        nocookie: false,
-        cc_load_policy: 1,
-        color: null,
-        controls: 1,
-        disablekb: 0,
-        enablejsapi: 0,
-        end: null,
-        fs: 1,
-        h1: null,
-        iv_load_policy: 1,
-        list: null,
-        listType: null,
-        loop: 0,
-        modestbranding: null,
-        origin: null,
-        playlist: null,
-        playsinline: null,
-        rel: 0,
-        showinfo: 1,
-        start: 0,
-        wmode: 'transparent',
-        theme: 'dark'
+  $('[data-fancybox]').fancybox({
+    loop: true,
+    animationEffect: "zoom",
+    transitionEffect: "fade",
+    transitionDuration: 366,
 
-      }
-    });*/
+    clickOutside: "close",
+  });
 
 
 
-  /*  $('.slider-for-recipe').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: false,
-      asNavFor: '.slider-nav-recipe',
-      fade: true,
-      draggable: false,
-      infinite: true,
+  $('.slider-review').slick({
 
-    });*/
-
-
-  /*  $('.slider-nav-recipe').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for-recipe',
-      arrows: false,
-      dots: false,
+      arrows: true,
+      dots: true,
       focusOnSelect: true,
       infinite: true,
-      centerMode: false,
-
-      responsive: [
-
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: true,
-
-        }
-      },
-
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-
-          arrows: true,
-        }
-      },
-
-      ]
 
     });
-    */
+    
 
-});
+  });
 
 
 
