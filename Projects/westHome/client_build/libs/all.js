9d1e4af323f736dcf1a28d2c0583b1e3ef970b91
DmitriyMK <alchimic@ua.fm>
2018-07-11 11:05:59 +0300

    fix

--- Projects/westHome/client_build/libs/all.js
+++ Projects/westHome/client_build/libs/all.js
@@ -64,16 +64,16 @@ $(document).ready(function() {
 
   $('.review-slider').slick({
 
-      arrows: true,
-      dots: true,
-      focusOnSelect: true,
-      infinite: false,
-      fade: true,
-
-    });
-    
+    arrows: true,
+    dots: true,
+    focusOnSelect: true,
+    infinite: false,
+    fade: true,
 
   });
+
+
+});
 
 
 
@@ -100,3 +100,25 @@ $(".form").submit(function() {
   });
   return false;
 });
+
+
+
+/*BE LAZY*/
+(function() {
+  if ('ontouchstart' in document.documentElement) {
+    document.querySelector('.js-ex').setAttribute('style', 'overflow-x: scroll;');
+    document.querySelector('.html-ex').setAttribute('style', 'overflow-x: scroll;');
+  }
+  var bLazy = new Blazy({
+    breakpoints: [{
+      width: 420, // max-width
+      src: 'data-src-small'
+    }],
+    success: function(element) {
+      setTimeout(function() {
+        var parent = element.parentNode;
+        parent.className = parent.className.replace(/\bloading\b/, '');
+      }, 200);
+    }
+  });
+})();
