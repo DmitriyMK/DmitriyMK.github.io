ee5a5aee8efa29a1963e1863c4ca6cc59a6c3d6f
DmitriyMK <alchimic@ua.fm>
2018-07-12 16:22:57 +0300

    fix

--- Projects/westHome/client_build/libs/map.js
+++ Projects/westHome/client_build/libs/map.js
@@ -26,140 +26,11 @@ function initMap() {
             style: google.maps.NavigationControlStyle.SMALL
         },
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        // styles:
-
-        //     [{
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#555b6b"
-        //     }]
-        // }, {
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#746855"
-        //     }]
-        // }, {
-        //     "elementType": "labels.text.stroke",
-        //     "stylers": [{
-        //         "color": "#242f3e"
-        //     }]
-        // }, {
-        //     "featureType": "administrative.locality",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#d59563"
-        //     }]
-        // }, {
-        //     "featureType": "poi",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#d59563"
-        //     }]
-        // }, {
-        //     "featureType": "poi.business",
-        //     "stylers": [{
-        //         "visibility": "off"
-        //     }]
-        // }, {
-        //     "featureType": "poi.park",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#263c3f"
-        //     }]
-        // }, {
-        //     "featureType": "poi.park",
-        //     "elementType": "labels.text",
-        //     "stylers": [{
-        //         "visibility": "off"
-        //     }]
-        // }, {
-        //     "featureType": "poi.park",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#6b9a76"
-        //     }]
-        // }, {
-        //     "featureType": "road",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#434a5b"
-        //     }]
-        // }, {
-        //     "featureType": "road",
-        //     "elementType": "geometry.stroke",
-        //     "stylers": [{
-        //         "color": "#434a5b"
-        //     }]
-        // }, {
-        //     "featureType": "road",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#adb0b7"
-        //     }]
-        // }, {
-        //     "featureType": "road.highway",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#746855"
-        //     }]
-        // }, {
-        //     "featureType": "road.highway",
-        //     "elementType": "geometry.stroke",
-        //     "stylers": [{
-        //         "color": "#1f2835"
-        //     }]
-        // }, {
-        //     "featureType": "road.highway",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#f3d19c"
-        //     }]
-        // }, {
-        //     "featureType": "transit",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#2f3948"
-        //     }]
-        // }, {
-        //     "featureType": "transit.station",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#d59563"
-        //     }]
-        // }, {
-        //     "featureType": "water",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#17263c"
-        //     }]
-        // }, {
-        //     "featureType": "water",
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "color": "#515c6d"
-        //     }]
-        // }, {
-        //     "featureType": "water",
-        //     "elementType": "labels.text.stroke",
-        //     "stylers": [{
-        //         "color": "#17263c"
-        //     }]
-        // }]
-
 
     };
 
-
-
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-/*    infoWindow = new google.maps.InfoWindow();
-
-    google.maps.event.addListener(map, "click", function() {
-        infoWindow.close();
-    });*/
-
-    // Определяем границы видимой области карты в соответствии с положением маркеров
     var bounds = new google.maps.LatLngBounds();
 
     for (var i = 0; i < markersData.length; i++) {
@@ -170,16 +41,12 @@ function initMap() {
 
         addMarker(latLng, name, address);
 
-        // Расширяем границы нашей видимой области, добавив координаты нашего текущего маркера
         bounds.extend(latLng);
     }
 
-    // Автоматически масштабируем карту так, чтобы все маркеры были в видимой области карты
-    /*map.fitBounds(bounds);*/
+    // map.fitBounds(bounds);
 
 }
-
-/*google.maps.event.addDomListener(initMap);*/
 
 function addMarker(latLng, name, address) {
 
