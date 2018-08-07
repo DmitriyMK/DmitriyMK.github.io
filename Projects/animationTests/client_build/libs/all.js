d25b654f35b61e36dde4ff24b0ddd7e18019aaae
DmitriyMK <alchimic@ua.fm>
2018-08-07 12:55:42 +0300

    add animationTest

---
+++ Projects/animationTests/client_build/libs/all.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+
+   if (document.documentElement.clientWidth > 1200) {
+
+    $("body").niceScroll({
+      scrollspeed: 70,
+      mousescrollstep: 60,
+      smoothscroll: true,
+      cursorwidth: 8,
+      cursorborder: 0,
+      cursorcolor: '#ff4200',
+      cursorborderradius: 2,
+      autohidemode: true,
+      horizrailenabled: false,
+      cursoropacitymin: 1,
+      background: false,
+      zindex: "9999"
+    });
+
+    $('html').addClass('no-overflow-y');
+
+  };
+});
