2c51b6a49291176aedf83c7d80f73085f6576fb4
DmitriyMK <alchimic@ua.fm>
2018-09-11 14:11:25 +0300

    fx

--- Projects/aurum/client_build/libs/map.js
+++ Projects/aurum/client_build/libs/map.js
@@ -1,64 +1,64 @@
-var markersData = [{
-        lat: 48.5075627,
-        lng: 34.9882175,
-        name: "AUrum",
-        address: "г. Днепр,  ул.  Шолохова, 7."
-    },
-];
+// var markersData = [{
+//         lat: 48.5075627,
+//         lng: 34.9882175,
+//         name: "AUrum",
+//         address: "г. Днепр,  ул.  Шолохова, 7."
+//     },
+// ];
 
-var map, infoWindow;
+// var map, infoWindow;
 
-function initMap() {
-    var centerLatLng = new google.maps.LatLng(48.5075627, 34.9902175);
+// function initMap() {
+//     var centerLatLng = new google.maps.LatLng(48.5075627, 34.9902175);
 
-    var mapOptions = {
-        center: centerLatLng,
-        zoom: 16,
+//     var mapOptions = {
+//         center: centerLatLng,
+//         zoom: 16,
 
-        mapTypeControl: true,
+//         mapTypeControl: true,
 
-        mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-        },
+//         mapTypeControlOptions: {
+//             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+//         },
 
-        scrollwheel: false,
-        disableDoubleClickZoom: true,
-        navigationControlOptions: {
-            style: google.maps.NavigationControlStyle.SMALL
-        },
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+//         scrollwheel: false,
+//         disableDoubleClickZoom: true,
+//         navigationControlOptions: {
+//             style: google.maps.NavigationControlStyle.SMALL
+//         },
+//         mapTypeId: google.maps.MapTypeId.ROADMAP,
 
-    };
+//     };
 
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var bounds = new google.maps.LatLngBounds();
+//     var bounds = new google.maps.LatLngBounds();
 
-    for (var i = 0; i < markersData.length; i++) {
+//     for (var i = 0; i < markersData.length; i++) {
 
-        var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
-        var name = markersData[i].name;
-        var address = markersData[i].address;
+//         var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+//         var name = markersData[i].name;
+//         var address = markersData[i].address;
 
-        addMarker(latLng, name, address);
+//         addMarker(latLng, name, address);
 
-        bounds.extend(latLng);
-    }
+//         bounds.extend(latLng);
+//     }
 
-    // map.fitBounds(bounds);
+//     // map.fitBounds(bounds);
 
-}
+// }
 
-function addMarker(latLng, name, address) {
+// function addMarker(latLng, name, address) {
 
-    var image = 'images/marker.png';
+//     var image = 'images/marker.png';
 
-    var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        icon: image,
-        title: name
-    });
+//     var marker = new google.maps.Marker({
+//         position: latLng,
+//         map: map,
+//         icon: image,
+//         title: name
+//     });
 
 
-}
+// }
