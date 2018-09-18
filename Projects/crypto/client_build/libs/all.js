0bdc1ffea3ed329243f8215a7a448bbd9452da0b
DmitriyMK <alchimic@ua.fm>
2018-09-18 18:36:29 +0300

    fx

--- Projects/crypto/client_build/libs/all.js
+++ Projects/crypto/client_build/libs/all.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     var t = 1500;
     var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
     $('html,body').stop().animate({
-      scrollTop: $(d).offset().top
+      scrollTop: $(d).offset().top - 70
     }, t);
   });
 
@@ -43,6 +43,7 @@ $(document).ready(function() {
     arrows: true,
     dots: true,
     infinite: false,
+    lazyLoad: 'ondemand',
     responsive: [
 
       {
@@ -89,34 +90,47 @@ $(document).ready(function() {
 
 
 window.onload = function() {
-    // get our canvas wrapper
-    var canvasContainer = document.getElementById("canvas");
+  // get our canvas wrapper
+  var canvasContainer = document.getElementById("canvas");
 
-    // set up our WebGL context and append the canvas to our wrapper
-    var webGLCurtain = new Curtains("canvas");
+  // set up our WebGL context and append the canvas to our wrapper
+  var webGLCurtain = new Curtains("canvas");
 
-    // get our plane element
-    var planeElement = document.getElementsByClassName("dancingDots")[0];
+  // get our plane element
+  var planeElement = document.getElementsByClassName("dancingDots")[0];
 
-    // set our initial parameters (basic uniforms)
-    var params = {
-      vertexShaderID: "plane-vs", // our vertex shader ID
-      fragmentShaderID: "plane-fs", // our framgent shader ID
+  // set our initial parameters (basic uniforms)
+  var params = {
+    vertexShaderID: "plane-vs", // our vertex shader ID
+    fragmentShaderID: "plane-fs", // our framgent shader ID
 
-      uniforms: {
-        time: {
-          name: "uTime", // uniform name that will be passed to our shaders
-          type: "1f", // this means our uniform is a float
-          value: 0,
-        },
-      }
+    uniforms: {
+      time: {
+        name: "uTime", // uniform name that will be passed to our shaders
+        type: "1f", // this means our uniform is a float
+        value: 0,
+      },
     }
+  }
 
-    // create our plane mesh
-    var plane = webGLCurtain.addPlane(planeElement, params);
+  // create our plane mesh
+  var plane = webGLCurtain.addPlane(planeElement, params);
 
-    // set up our basic methods
-    plane.onRender(function() { // fired at each requestAnimationFrame call
-      plane.uniforms.time.value++; // update our time uniform value
-    });
+  // set up our basic methods
+  plane.onRender(function() { // fired at each requestAnimationFrame call
+    plane.uniforms.time.value++; // update our time uniform value
+  });
 };
+
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 600) {
+    $('#headerTop').addClass('fixedMenu')
+  } else if ($(this).scrollTop() < 600) {
+    $('#headerTop').removeClass('fixedMenu')
+  } else if ($(this).scrollTop() > 10) {
+    $('#headerTop').removeClass('fixedMenuUps')
+  } else if ($(this).scrollTop() < 10) {
+    $('#headerTop').addClass('fixedMenuUps');
+  };
+});
