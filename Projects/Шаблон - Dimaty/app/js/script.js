e4148d7d6fb09df013b663f1c149f1bcb317f758
Dmitriy (alchimic) <alchimic@ua.fm>
2018-12-20 00:12:22 +0200

    template Dimaty

--- Projects/Шаблон - Dimaty/app/js/script.js
+++ Projects/Шаблон - Dimaty/app/js/script.js
@@ -1,170 +1,393 @@
 $(document).ready(function() {
 
-  $("body").niceScroll({
-    scrollspeed: 60,
-    mousescrollstep: 60,
-    smoothscroll: true,
-    cursorwidth: 10,
-    cursorborder: 0,
-    cursorcolor: '#55c202',
-    cursorborderradius: 2,
-    autohidemode: true,
-    horizrailenabled: false,
-    cursoropacitymin: 1,
-    background: false,
-    zindex: "9999"
-  });
+  if (document.documentElement.clientWidth > 1200) {
 
-  $('html').addClass('no-overflow-y');
+    // $(window).enllax();
 
-});
+    // $("body").niceScroll({
+    //   scrollspeed: 60,
+    //   mousescrollstep: 60,
+    //   smoothscroll: true,
+    //   cursorwidth: 8,
+    //   cursorborder: 0,
+    //   cursorcolor: '#181d23',
+    //   cursorborderradius: 2,
+    //   autohidemode: true,
+    //   horizrailenabled: false,
+    //   cursoropacitymin: 1,
+    //   background: false,
+    //   zindex: "9999"
+    // });
 
-
-
-
+    // $('html').addClass('no-overflow-y');
 
 
-/*FIXED SCROLL FOR MENU*/
-/*$(document).ready(function() {
-
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 600) {
-      $('#fixedTopMenu').addClass('fixed');
-    } else if ($(this).scrollTop() < 600) {
-      $('#fixedTopMenu').removeClass('fixed');
-    }
-  });
-
-});*/
+    // var els = document.querySelectorAll(".js-splitme");
+    // [].forEach.call(els, function(el) {
+    //   el.outerHTML = Splitter(el.outerHTML, '<span class="varter">$</span>');
+    // });
 
 
-/*FIXED SCROLL TO TOP*/
-/*$(document).ready(function() {
+    // SCROLL ANIMATION
+    // $('[data-scrollmagic1]').each(function(index, elem) {
+    //   var scrollMagicController = new ScrollMagic();
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 400) {
-      $('.scrollup').fadeIn();
-    } else {
-      $('.scrollup').fadeOut();
-    }
-  });
+    //   // Create Animations
+    //   var
+    //   img1 = $(elem).find('.statistics__item1'),
+    //   img2 = $(elem).find('.statistics__item2'),
+    //   img3 = $(elem).find('.statistics__item3'),
+    //   img4 = $(elem).find('.statistics__item4');
 
-  $('.scrollup').click(function() {
-    $("html, body").animate({
-      scrollTop: 0
-    }, 600);
-    return false;
-  });
+    //   var tl = new TimelineMax({
+    //     pause: true
+    //   });
 
-});*/
+    //   tl.add("start")
 
-$(document).ready(function() {
+    //   .fromTo(img1, 1.2, {y: '80px', opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut}, "start")
+    //   .fromTo(img2, 1.6, {y: '80px', opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut}, "start")
+    //   .fromTo(img3, 2.0, {y: '80px', opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut}, "start")
+    //   .fromTo(img4, 2.4, {y: '80px', opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut}, "start")
 
-/*  if (document.documentElement.clientWidth > 1200) {
-    new WOW().init();
+
+    //   var scene = new ScrollScene({
+    //     triggerElement: elem,
+    //     reverse: false,
+    //     offset: -440
+    //   })
+
+    //   .setTween(tl)
+    //   .addTo(scrollMagicController);
+    // });
+    // END data-scrollmagic1
 
   };
 
-  $('.srolling__link').on('click', function(e) {
-    e.preventDefault();
-    var t = 1500;
-    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
-    $('html,body').stop().animate({
-      scrollTop: $(d).offset().top
-    }, t);
-  });*/
-
-  /*LAZY LOAD*/
-/*  $('.lazy').lazy({
-    effect: "fadeIn",
-    effectTime: 200,
-    threshold: 0,
-    scrollDirection: 'vertical',
-    visibleOnly: true,
-
-    onError: function(element) {
-      console.log('error loading ' + element.data('src'));
-    }
-
-  });
-*/
-
-/*  $('.phone__mask').mask('+38(999)999-99-99');
 
 
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-  });*/
+  // BURGER RESPONSIVE < 992px
+  // $('.burger').click(function() {
+  //   $(this).toggleClass('active');
+  //   $(this).parents('.header__top').find('.overlay').toggleClass('open');
+  // });
+
+
+  // $(".linkSubmenu").hover(function() {
+  //   if (document.documentElement.clientWidth > 992) {
+  //     // $(this).find(".nav__hide").toggleClass("nav__show");
+  //     $(this).find(".nav__link").toggleClass("nav__link-active");
+  //   };
+  // });
+
+
+  // $('.linkSubmenu').click(function() {
+  //   if (document.documentElement.clientWidth < 992) {
+
+  //     $(this).find(".nav__hide").toggleClass("nav__show");
+  //     $(this).find(".nav__link").toggleClass("nav__link-active");
+
+  //   };
+  // });
 
 
 
-
-/*  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrow: true,
-    asNavFor: '.slider-nav',
-    fade: true
-  });
+  // var bLazy = new Blazy();
 
 
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    arrows: false,
-    dots: false,
-    focusOnSelect: true,
-    responsive: [
+  // $('.recipePage__slider').slick({
 
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
-          dots: true,
-        }
-      }
+  //   arrow: true,
+  //   dots: true,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   rows: 2,
+  //   infinite: false,
+  //   lazyLoad: 'ondemand',
 
-    ]
-  });*/
+  //   responsive: [
+
+  //   {
+  //     breakpoint: 1100,
+  //     settings: 
+  //     {
+  //       slidesToShow: 3,
+  //       rows: 2,
+  //     }
+  //   }, 
+
+  //   {
+  //     breakpoint: 992,
+  //     settings: 
+  //     {
+  //       slidesToShow: 2,
+  //       rows: 1,
+  //     }
+  //   },
+
+  //   {
+  //     breakpoint: 600,
+  //     settings: 
+  //     { 
+  //       slidesToShow: 1,
+  //       rows: 1,
+  //     }
+  //   },
+
+
+  //   ]
+  // });
+
+
+
+  // $('.productSlider-big').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   infinite: false,
+  //   draggable: false,
+  //   asNavFor: '.productSlider-small'
+  // });
+
+
+  // $('.productSlider-small').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.productSlider-big',
+  //   dots: false,
+  //   infinite: false,
+  //   // centerMode: true,
+  //   focusOnSelect: true
+  // });
+
 
 
 });
 
 
-
-/*$(window).on('resize orientationchange', function() {
-  $('.slider').slick('resize');
-});
-*/
-
-
-
-// $(".form").submit(function() {
-//   var th = $(this);
-//   $.ajax({
-//     type: "POST",
-//     url: "mail.php",
-//     data: th.serialize()
-//   }).done(function() {
-
-//     var inst = $('[data-remodal-id=modal-thanks]').remodal();
-//     inst.open();
-
-//     setTimeout(function() {
-//       th.trigger("reset");
-//     }, 1000);
-//   });
-//   return false;
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 30) {
+//     $('#header').addClass('fixed');
+//   } else if ($(this).scrollTop() < 30) {
+//     $('#header').removeClass('fixed');
+//   }
 // });
 
 
 
+// FORMS 
+// $("#feedbackForm").validate({
 
-/*PRELOADER*/
-$(window).load(function() {
-	$('#preloader').delay(100).fadeOut('slow');
+//   rules: {
+//     name: "required",
+//     email: {
+//       required: true,
+//       email: true,
+//     },
+//     phone: {
+//       required: true,
+//     }
 
-});
+//   },
+//   messages: {
+//     name: "Введите как минимум два символа",
+//     email: "Проверьте правильность ввода",
+//     phone: "Введите корректный номер телефона",
+//     message: "Поле для сообщения не может быть пустым"
+//   },
+
+//   submitHandler: function() {
+//     var th = $("#feedbackForm");
+
+//     $.ajax({
+//       type: "POST",
+//       url: "mail.php",
+//       data: th.serialize()
+//     }).done(function() {
+
+//       // window.location = 'thank.html';
+
+//       setTimeout(function() {
+//         th.trigger("reset");
+//       }, 1000);
+//     });
+
+//     return false;
+//   }
+// });
+
+
+// $("#ServicesForm").validate({
+
+//   rules: {
+//     name: "required",
+//     email: {
+//       required: true,
+//       email: true,
+//     },
+//     phone: {
+//       required: true,
+//     },
+//     typeEvents: {
+//       required: true,
+//     }
+//   },
+
+//   messages: {
+//     name: "Введите как минимум два символа",
+//     email: "Проверьте правильность ввода",
+//     phone: "Введите корректный номер телефона",
+//     typeEvents: "Выберите вид мероприятия",
+//     guest: "Введите количество гостей",
+//     budget: "Введите сумму Вашего бюджета"
+//   },
+
+//   submitHandler: function() {
+//     var th = $("#feedbackForm");
+
+//     $.ajax({
+//       type: "POST",
+//       url: "mail.php",
+//       data: th.serialize()
+//     }).done(function() {
+
+//       // window.location = 'thank.html';
+
+//       setTimeout(function() {
+//         th.trigger("reset");
+//       }, 1000);
+//     });
+
+//     return false;
+//   }
+// });
+
+
+
+// SELECT FUNCTION
+// var x, i, j, selElmnt, a, b, c;
+
+// x = document.getElementsByClassName("custom-select");
+// for (i = 0; i < x.length; i++) {
+//   selElmnt = x[i].getElementsByTagName("select")[0];
+
+//   a = document.createElement("DIV");
+//   a.setAttribute("class", "select-selected");
+//   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+//   x[i].appendChild(a);
+
+//   b = document.createElement("DIV");
+//   b.setAttribute("class", "select-items select-hide");
+//   for (j = 1; j < selElmnt.length; j++) {
+
+//     c = document.createElement("DIV");
+//     c.innerHTML = selElmnt.options[j].innerHTML;
+//     c.addEventListener("click", function(e) {
+
+//       var y, i, k, s, h;
+//       s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+//       h = this.parentNode.previousSibling;
+//       for (i = 0; i < s.length; i++) {
+//         if (s.options[i].innerHTML == this.innerHTML) {
+//           s.selectedIndex = i;
+//           h.innerHTML = this.innerHTML;
+//           y = this.parentNode.getElementsByClassName("same-as-selected");
+//           for (k = 0; k < y.length; k++) {
+//             y[k].removeAttribute("class");
+//           }
+//           this.setAttribute("class", "same-as-selected");
+//           break;
+//         }
+//       }
+//       h.click();
+//     });
+//     b.appendChild(c);
+//   }
+//   x[i].appendChild(b);
+//   a.addEventListener("click", function(e) {
+
+//     e.stopPropagation();
+//     closeAllSelect(this);
+//     this.nextSibling.classList.toggle("select-hide");
+//     this.classList.toggle("select-arrow-active");
+//   });
+// }
+// function closeAllSelect(elmnt) {
+
+//   var x, y, i, arrNo = [];
+//   x = document.getElementsByClassName("select-items");
+//   y = document.getElementsByClassName("select-selected");
+//   for (i = 0; i < y.length; i++) {
+//     if (elmnt == y[i]) {
+//       arrNo.push(i)
+//     } else {
+//       y[i].classList.remove("select-arrow-active");
+//     }
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     if (arrNo.indexOf(i)) {
+//       x[i].classList.add("select-hide");
+//     }
+//   }
+// }
+
+// document.addEventListener("click", closeAllSelect);
+// END  SELECT FUNCTION
+
+
+// FUNCTION FOR NUMBER
+// (function() {
+
+//   window.inputNumber = function(el) {
+
+//     var min = el.attr('min') || false;
+//     var max = el.attr('max') || false;
+
+//     var els = {};
+
+//     els.dec = el.prev();
+//     els.inc = el.next();
+
+//     el.each(function() {
+//       init($(this));
+//     });
+
+//     function init(el) {
+
+//       els.dec.on('click', decrement);
+//       els.inc.on('click', increment);
+
+//       function decrement() {
+//         var value = el[0].value;
+//         value--;
+//         if(!min || value >= min) {
+//           el[0].value = value;
+//         }
+//       }
+
+//       function increment() {
+//         var value = el[0].value;
+//         value++;
+//         if(!max || value <= max) {
+//           el[0].value = value++;
+//         }
+//       }
+//     }
+//   }
+// })();
+
+// inputNumber($('.input-number'));
+// END FUNCTION FOR NUMBER
+
+
+// TABS
+// $('.tabs__link').click(function() {
+//   var tabID = $(this).attr('data-tabs');
+
+//   $(this).addClass('tabs__link-active').siblings().removeClass('tabs__link-active');
+//   $('#tabs-' + tabID).addClass('tabs__content-active').siblings().removeClass('tabs__content-active');
+// });
+
+
+window.console.log('Made with fun and love ❤️ Push-K solutions ❤️');
--- Projects/Шаблон - Dimaty/gulpfile.js
+++ Projects/Шаблон - Dimaty/gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     sass = require('gulp-sass'),
-    bourbon = require('node-bourbon'),
-    browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs'),
     cssnano = require('gulp-cssnano'),
+    cleancss = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    bourbon = require('node-bourbon'),
+    uglify = require('gulp-uglifyjs'),
+
+    browserSync = require('browser-sync'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
-    cache = require('gulp-cache'),  
-    autoprefixer = require('gulp-autoprefixer');
+    cache = require('gulp-cache'),
+
+    del = require('del'),
+    notify = require("gulp-notify");
+//    rsync = require('gulp-rsync');
+
 
 
 gulp.task('sass', function() {
@@ -18,8 +25,22 @@ gulp.task('sass', function() {
         .pipe(sass({
             includePaths: require('node-bourbon').includePaths
         }))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8'], {
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on("error", notify.onError()))
+        .pipe(rename({
+            suffix: '.min',
+            prefix: ''
+        }))
+        .pipe(autoprefixer(['last 5 versions', '> 1%', 'ie 8'], {
             cascade: true
+        }))
+        .pipe(cleancss({
+            level: {
+                1: {
+                    specialComments: 0
+                }
+            }
         }))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
@@ -27,54 +48,61 @@ gulp.task('sass', function() {
         }))
 });
 
+
 gulp.task('browser-sync', function() {
-    browserSync({ 
-        server: { 
-            baseDir: 'app' 
+    browserSync({
+        server: {
+            baseDir: 'app'
         },
-        notify: true 
+        notify: false,
+        // open: false,
+        // online: false, // Work Offline Without Internet Connection
+        // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
     });
 });
 
+
 gulp.task('scripts', function() {
     return gulp.src([
-           
-            'app/js/libs/waypoints.min.js',
-            'app/js/libs/jquery.counterup.min.js',
-            'app/js/libs/jquery.lazy.min.js',
-            'app/js/libs/masked-input.js',
-            'app/js/libs/jquery.validate.min.js',
-            'app/js/libs/wow.min.js',
-            'app/js/libs/remodal.min.js',
+
+            'app/js/script.js',
 
         ])
         .pipe(concat('script.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
+
 gulp.task('css-libs', ['sass'], function() {
-    return gulp.src('app/css/main.css') 
-        .pipe(cssnano()) 
+    return gulp.src('app/css/main.css')
+        .pipe(cssnano())
         .pipe(rename({
             suffix: '.min'
-        })) 
+        }))
         .pipe(gulp.dest('app/css'));
 });
 
+
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']); 
-    gulp.watch('app/*.html', browserSync.reload); 
-    gulp.watch('app/js/**/*.js', browserSync.reload); 
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+
 
 gulp.task('clean', function() {
-    return del.sync('dist'); 
+    return del.sync('dist');
 });
 
+
 gulp.task('img', function() {
-    return gulp.src('app/img/**/*') 
-        .pipe(cache(imagemin({ 
+    return gulp.src('app/img/**/*')
+        .pipe(cache(imagemin({
             interlaced: true,
             progressive: true,
             svgoPlugins: [{
@@ -82,32 +110,35 @@ gulp.task('img', function() {
             }],
             use: [pngquant()]
         })))
-        .pipe(gulp.dest('dist/img')); 
+        .pipe(gulp.dest('dist/img'));
 });
 
+
 gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
 
     var buildFiles = gulp.src([
         'app/*.html',
         'app/.htaccess',
-        ]).pipe(gulp.dest('dist'));
+    ]).pipe(gulp.dest('dist'));
 
     var buildCss = gulp.src([
         'app/css/main.min.css',
-        ]).pipe(gulp.dest('dist/css'));
+    ]).pipe(gulp.dest('dist/css'));
 
     var buildJs = gulp.src([
         'app/js/script.min.js',
-        ]).pipe(gulp.dest('dist/js'));
+    ]).pipe(gulp.dest('dist/js'));
 
     var buildFonts = gulp.src([
         'app/fonts/**/*',
-        ]).pipe(gulp.dest('dist/fonts'));
+    ]).pipe(gulp.dest('dist/fonts'));
 
 });
 
 gulp.task('clear', function() {
     return cache.clearAll();
 })
+
 
 gulp.task('default', ['watch']);
